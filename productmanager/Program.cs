35be86da95aa1dd3da26e5dfa63dbbe285ea312f
mlastra01 <mlastra@est.frba.utn.edu.ar>
2021-03-22 02:43:27 -0300

    fcommit

---
+++ productmanager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace productmanager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
